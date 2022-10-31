39033868b1d6d6d199d009628ca5b522b3742f75
Carolina Nogueira <carolina.tinen@alumni.usp.br>
2022-10-31 19:28:10 -0300

    teste atualização

--- banco_sala-2/app.js
+++ banco_sala-2/app.js
@@ -6,7 +6,7 @@ import { rotaPrincipal } from './src/aplicacao/routes/routes.js';
 
 const app = express();
 app.use(rotaPrincipal);
-const PORT = config().parsed.PORT;
+//const PORT = config().parsed.PORT;
 
 app.listen(process.env.PORT, () => {
     console.log(`Acessar http://localhost:${process.env.PORT}/paginainicial \r\n
